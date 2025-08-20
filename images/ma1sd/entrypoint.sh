1463960994d12cbcb8248495c34d77a397e91242
Milan Mertens <m.mertens@metaways.de>
2025-08-20 11:00:08 +0200

    ma1sd image: adds config templating

---
+++ images/ma1sd/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [[ -f /etc/ma1sd/ma1sd.yaml.tmpl ]]; then
+    gomplate -i /etc/ma1sd/ma1sd.yaml.tmpl -o /etc/ma1sd/ma1sd.yaml
+fi
+
+exec java -jar /app/ma1sd.jar -c /etc/ma1sd/ma1sd.yaml
